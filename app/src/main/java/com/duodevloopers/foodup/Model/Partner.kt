5665a3ffa70b57275f9adc3ec9455e4d7c5511a6
Niaz Sagor <niazsagor@gmail.com>
2021-12-27 16:34:09 +0600

    bugss

--- app/src/main/java/com/duodevloopers/foodup/Model/Partner.kt
+++ app/src/main/java/com/duodevloopers/foodup/Model/Partner.kt
@@ -1,12 +1,29 @@
 package com.duodevloopers.foodup.Model
 
-class Partner(
-    var details: String,
-    var image: String,
-    var name: String,
-    var ownerName: String,
-    var phoneNumber: String,
-    var type: String
-) {
+class Partner{
+
+    lateinit var details: String
+    lateinit var image: String
+    lateinit var name: String
+    lateinit var ownerName: String
+    lateinit var phoneNumber: String
+    lateinit var type: String
+
     constructor()
+
+    constructor(
+        details: String,
+        image: String,
+        name: String,
+        ownerName: String,
+        phoneNumber: String,
+        type: String
+    ) {
+        this.details = details
+        this.image = image
+        this.name = name
+        this.ownerName = ownerName
+        this.phoneNumber = phoneNumber
+        this.type = type
+    }
 }
